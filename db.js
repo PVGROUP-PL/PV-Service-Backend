dca88eaf29d328ae3877c159cd128a8b6a074560
pakowanko <wojtek@pakowanko.com>
2025-07-10 23:07:37 +0200

    FIX: Poprawiono konfigurację połączenia z bazą danych i zmienne środowiskowe

--- db.js
+++ db.js
@@ -1,26 +1,29 @@
 // db.js
+
+// --- POCZĄTEK SEKCJI DEBUGOWANIA ---
+console.log('--- START db.js: Sprawdzanie zmiennych środowiskowych ---');
+console.log('Zmienna NODE_ENV:', process.env.NODE_ENV);
+console.log('Zmienna INSTANCE_CONNECTION_NAME:', process.env.INSTANCE_CONNECTION_NAME);
+console.log('Zmienna DB_USER:', process.env.DB_USER);
+console.log('Zmienna DB_NAME:', process.env.DB_NAME);
+console.log('--- KONIEC SEKCJI DEBUGOWANIA ---');
+// --- KONIEC SEKCJI DEBUGOWANIA ---
+
+
 const { Pool } = require('pg');
 
-const isProduction = process.env.NODE_ENV === 'production';
-// Ta zmienna jest automatycznie dodawana przez Google, gdy skonfigurujesz połączenie Cloud SQL
 const INSTANCE_CONNECTION_NAME = process.env.INSTANCE_CONNECTION_NAME; 
-
 const config = {};
 
-if (isProduction && INSTANCE_CONNECTION_NAME) {
-  // --- Konfiguracja dla Cloud Run (PRODUKCJA) ---
-  console.log(`Łączenie z bazą danych przez Unix Socket: /cloudsql/${INSTANCE_CONNECTION_NAME}`);
+if (INSTANCE_CONNECTION_NAME) {
+  console.log(`Znaleziono INSTANCE_CONNECTION_NAME. Łączenie z bazą przez Unix Socket...`);
   config.host = `/cloudsql/${INSTANCE_CONNECTION_NAME}`;
-  config.user = process.env.DB_USER;     // np. 'postgres'
-  config.password = process.env.DB_PASS; // Hasło do bazy
-  config.database = process.env.DB_NAME; // Nazwa bazy
+  config.user = process.env.DB_USER;
+  config.password = process.env.DB_PASS;
+  config.database = process.env.DB_NAME;
 } else {
-  // --- Konfiguracja dla dewelopera (LOKALNIE) ---
-  console.log('Łączenie z bazą danych przez DATABASE_URL.');
+  console.log('Brak INSTANCE_CONNECTION_NAME. Łączenie z bazą przez DATABASE_URL (dla dewelopera).');
   config.connectionString = process.env.DATABASE_URL;
-  if (isProduction) { // Dodajemy SSL dla innych środowisk produkcyjnych (np. Heroku)
-      config.ssl = { rejectUnauthorized: false };
-  }
 }
 
 const pool = new Pool(config);
