635419ff1179ba3a007c2edca4053fc24d2ea4dd
pakowanko <wojtek@pakowanko.com>
2025-07-11 07:21:22 +0200

    FIX: Finalna konfiguracja i instalacja CORS

--- db.js
+++ db.js
@@ -1,31 +1,14 @@
 // db.js
-
-// --- POCZĄTEK SEKCJI DEBUGOWANIA ---
-console.log('--- START db.js: Sprawdzanie zmiennych środowiskowych ---');
-console.log('Zmienna NODE_ENV:', process.env.NODE_ENV);
-console.log('Zmienna INSTANCE_CONNECTION_NAME:', process.env.INSTANCE_CONNECTION_NAME);
-console.log('Zmienna DB_USER:', process.env.DB_USER);
-console.log('Zmienna DB_NAME:', process.env.DB_NAME);
-console.log('--- KONIEC SEKCJI DEBUGOWANIA ---');
-// --- KONIEC SEKCJI DEBUGOWANIA ---
-
-
 const { Pool } = require('pg');
 
-const INSTANCE_CONNECTION_NAME = process.env.INSTANCE_CONNECTION_NAME; 
-const config = {};
+console.log('Łączenie z bazą danych przez DATABASE_URL...');
 
-if (INSTANCE_CONNECTION_NAME) {
-  console.log(`Znaleziono INSTANCE_CONNECTION_NAME. Łączenie z bazą przez Unix Socket...`);
-  config.host = `/cloudsql/${INSTANCE_CONNECTION_NAME}`;
-  config.user = process.env.DB_USER;
-  config.password = process.env.DB_PASS;
-  config.database = process.env.DB_NAME;
-} else {
-  console.log('Brak INSTANCE_CONNECTION_NAME. Łączenie z bazą przez DATABASE_URL (dla dewelopera).');
-  config.connectionString = process.env.DATABASE_URL;
-}
-
-const pool = new Pool(config);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // SSL jest wymagany przez Google Cloud SQL dla połączeń publicznych
+  ssl: { 
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = pool;
